c56d821814f6ba1307ef45a37b3d7e2baf1680e7
Agustin <aguspulido14@gmail.com>
2021-11-26 20:53:02 -0300

    app.module reorganized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// Services
 import { JsonService} from '../services/json-service';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+
+// Components
+import { AppComponent } from './app.component';
 import { ObservablesComponent } from './observables/observables.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTabsModule } from '@angular/material/tabs';
 import { SubscribeComponent } from './subscribe/subscribe.component';
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from '@angular/material/input';
 import { ApicallComponent } from './apicall/apicall.component';
-import { SharedModule } from "../shared/shared.module";
 import { DirectivasComponent } from './directivas/directivas.component';
 import { FooterComponent } from './footer/footer.component';
+
+// Modules
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [
