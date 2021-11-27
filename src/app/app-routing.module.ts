1a6e7223c6bfc0c7cb04e982b25db3c98b3012e2
Agustin <aguspulido14@gmail.com>
2021-11-26 21:17:38 -0300

    Routing not necessary

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ObservablesComponent } from './observables/observables.component'
-import { AppComponent } from './app.component'
 
-const routes: Routes = [
-  {
-    // Ejemplos de observables para afterclass
-    path: "observables",
-    component: ObservablesComponent,
-  },
-  {
-    // AppComponent / ejemplos varios
-    path: "home",
-    component: AppComponent,
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
